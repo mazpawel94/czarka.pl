a8a05efc333863a1e778b783e346b76af0a3a72e
mazpawel94 <mazpawel94@gmail.com>
2019-03-27 12:12:29 +0100

    refactor - remove jQuery function, change let to const, reorganize same functions

--- reservation.js
+++ reservation.js
@@ -1,109 +1,96 @@
-$('#date').dateDropper();
+$("#date").dateDropper();
 $("#time").timeDropper();
-const inputDate = document.getElementById('date');
-const inputHour = document.getElementById('time');
+
+const inputDate = document.getElementById("date");
+const inputHour = document.getElementById("time");
 const today = new Date().toJSON().slice(0, 10);
 const actualTime = new Date().toJSON().slice(11, 16);
-const calendarPage = document.getElementById('datedropper-0');
-const clock = document.querySelector('.td-clock');
-const dateFromBase = document.querySelector('.dateFromBase');
-const minutesToAround = document.querySelectorAll('.td-time>span')[1];
-const rotateClock = document.querySelector('.td-n');
+const calendarPage = document.getElementById("datedropper-0");
+const clock = document.querySelector(".td-clock");
+const minutesToAround = document.querySelectorAll(".td-time>span")[1];
+const rotateClock = document.querySelector(".td-n");
+const tables = document.querySelectorAll(".table");
 
+const reservations = [];
 
-const reservation = [];
+const compareHours = (pickedHour, elementHour) => {
+  const pickedMinutes =
+    parseInt(pickedHour.slice(0, 2)) * 60 + parseInt(pickedHour.slice(3, 5));
+  const elementMinutes =
+    parseInt(elementHour.slice(0, 2)) * 60 + parseInt(elementHour.slice(3, 5));
+  if (pickedMinutes < elementMinutes || pickedMinutes > elementMinutes + 180)
+    return false;
+  return true;
+};
 
-function compareHours(pickedHour, elementHour) {
-    let pickedMinutes = Number(pickedHour.slice(0,2))*60 + Number(pickedHour.slice(3,5));
-    let elementMinutes = Number(elementHour.slice(0,2))*60 + Number(elementHour.slice(3,5));
-    if(pickedMinutes<elementMinutes || pickedMinutes>(elementMinutes + 180))
-        return false;
-    else
-        return true;
-}
+const endReservation = startReservation => {
+  const end = parseInt(startReservation.slice(0, 2)) + 3;
+  return end + startReservation.slice(2);
+};
 
-function endReservation(startReservation) {
-    const end = Number(startReservation.slice(0,2))+3;
-    return end+startReservation.slice(2,);
-}
+const setFreeTables = () =>
+  [...tables].forEach(table => table.classList.remove("busy", "busySoon"));
 
-function setFreeTables() {
-    $('.table').removeClass('busy');
-    $('.table').removeClass('busySoon');
-}
+const addThreeHours = hours => {
+  const trueHour = parseInt(hours.slice(0, 2));
+  const minutes = hours.slice(3, 5);
+  const newHour = trueHour + 3;
+  if (newHour < 10) newHour = `0${newHour}`;
+  return `${newHour}:${minutes}`;
+};
 
-function addThreeHours(hours) {
-    let trueHour = Number(hours.slice(0,2));
-    let minutes =hours.slice(3,5);
-    let newHour = trueHour+3;
-    if (newHour<10)
-        newHour = `0${newHour}`;
-    return `${newHour}:${minutes}`;
-}
-
-function aroundMinutes() {
-    if(minutesToAround.classList.contains('on')) {
-        let n =Math.round((minutesToAround.innerHTML)/15)*15;
-        console.log(n);
-        let radius = n*6;
-        rotateClock.style.transform = `rotate(${radius}deg)`;
-       
-        if(n<15 || n==60)
-        {
-            inputHour.value = `${inputHour.value.slice(0,3)}00`;
-            minutesToAround.innerHTML =`00`;
-            minutesToAround.setAttribute('data-id', '00');
-        }
-           
-        else {
-            inputHour.value = `${inputHour.value.slice(0,3)}${n}`;
-            minutesToAround.innerHTML =n;
-            minutesToAround.setAttribute('data-id', n);
-        }
-          
-    }
-
-}
+const aroundMinutes = () => {
+  if (!minutesToAround.classList.contains("on")) return;
+  const n = Math.round(minutesToAround.innerHTML / 15) * 15;
+  const radius = n * 6;
+  rotateClock.style.transform = `rotate(${radius}deg)`;
+  if (n < 15 || n == 60) {
+    inputHour.value = `${inputHour.value.slice(0, 3)}00`;
+    minutesToAround.innerHTML = `00`;
+    minutesToAround.setAttribute("data-id", "00");
+  } else {
+    inputHour.value = `${inputHour.value.slice(0, 3)}${n}`;
+    minutesToAround.innerHTML = n;
+    minutesToAround.setAttribute("data-id", n);
+  }
+};
 
 function showReservation() {
-    setFreeTables();
-    let pickedDay = inputDate.value;
-    let pickedHour = inputHour.value;
-    if(pickedHour.length <= 4)
-        pickedHour=`0${pickedHour}`;
-    let i = 1;
-    reservation.forEach(element => {
-        if(element.date == pickedDay && compareHours(pickedHour, element.hour))
-            {
-                const reserve = element.table;
-                $(`.${reserve}`).addClass('busy')
-                .attr('data-busy', `Rezerwacja \ ${element.hour} - ${endReservation(element.hour)}`);
-            }
-        else if(element.date == pickedDay && compareHours(addThreeHours(pickedHour), element.hour))
-            {
-                const reserve = element.table;
-                $(`.${reserve}`).addClass('busySoon')
-                .attr('data-busy-soon', ` Najbliższa rezerwacja \ ${element.hour}`);
-            }
-        })
+  setFreeTables();
+  const pickedDay = inputDate.value;
+  const pickedHour = inputHour.value;
+  if (pickedHour.length <= 4) pickedHour = `0${pickedHour}`;
+  reservations.forEach(reservation => {
+    if (
+      reservation.date == pickedDay &&
+      compareHours(pickedHour, reservation.hour)
+    ) {
+      const table = document.querySelector(`.${reservation.table}`);
+      table.classList.add("busy");
+      table.dataset.busy = `Rezerwacja \ ${reservation.hour} - ${endReservation(
+        reservation.hour
+      )}`;
+    } else if (
+      reservation.date == pickedDay &&
+      compareHours(addThreeHours(pickedHour), reservation.hour)
+    ) {
+      const table = document.querySelector(`.${reservation.table}`);
+      table.classList.add("busy-soon");
+      table.dataset.busySoon = ` Najbliższa rezerwacja \ ${reservation.hour}`;
+    }
+  });
 }
-showReservation();
-calendarPage.addEventListener('click', showReservation);
-clock.addEventListener('click', showReservation);
-clock.addEventListener('click', aroundMinutes);
-clock.addEventListener('touchmove', showReservation);
+calendarPage.addEventListener("click", showReservation);
+clock.addEventListener("click", showReservation);
+clock.addEventListener("click", aroundMinutes);
+clock.addEventListener("touchmove", showReservation);
 
 let xhr = new XMLHttpRequest();
-xhr.open(
-    "GET",
-    "https://czarka-api.herokuapp.com/reservations",
-    true
-  );
+xhr.open("GET", "https://czarka-api.herokuapp.com/reservations", true);
 xhr.addEventListener("load", function() {
-    const date = JSON.parse(this.responseText);
-    console.log(reservation);
-    [...date].forEach(e => reservation.push(e));
-    console.log(reservation);
+  const date = JSON.parse(this.responseText);
+  [...date].forEach(e => reservations.push(e));
+  showReservation();
 });
 
 xhr.send();
